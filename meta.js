24ced7be7e37b338fb24dcb2c5b2a2d4cf3840bb
matheus.vidal <matheus.vidal@buser.com.br>
2021-10-16 20:33:52 -0300

    teste

--- meta.js
+++ meta.js
@@ -1,33 +1,24 @@
-var fs = require('fs');
-
-function onerror(err) {
-    if ( err ) console.log('ERROR: ' + err);
-}
-
 module.exports = {
-  "prompts": {
-    "name": {
-      "type": "string",
-      "required": true,
-      "label": "Project name"
+  prompts: {
+    name: {
+      type: 'string',
+      required: true,
+      message: 'Project name'
+    },
+    description: {
+      type: 'string',
+      required: false,
+      message: '',
+      default: 'My Awesome Project'
+    },
+    author: {
+      type: 'string',
+      message: 'Author'
+    },
+    extra: {
+      type: 'string',
+      message: 'Extra Messaging!'
     }
   },
-  "skipInterpolation": "frontend/**/*.vue",
-  // "completeMessage": "{{#inPlace}}To get started:\n\n  npm install\n  npm run dev.{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev.{{/inPlace}}",
-  "complete": function (data, {logger, chalk}) {
-    const base = data.inPlace ? '' : data.destDirName + '/'
-    fs.rename(`${base}__name__`, `${base}${data.name}`, onerror)
-    cmds = data.inPlace ? '' : `cd ${data.destDirName}\n  `
-    cmds += `follow the instructions on README.md
-  (or see https://github.com/evolutio/djavue/blob/master/template/README.md)`
-    const message = `
-# ${chalk.green('Project initialization finished!')}
-# ========================
-To get started:
-  
-  ${chalk.yellow(cmds)}
-Documentation can be found at https://github.com/evolutio/djavue
-`
-    console.log(message)
-  },
-}
+  completeMessage: 'Project Complete!'
+};
